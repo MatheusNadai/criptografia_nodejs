4e46e445297b14779adda73a34aa16b37fc7dc48
Matheus de Nadai <matheus.nadai@hotmail.com>
2021-06-12 13:00:51 -0300

    Finish Crypto

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const password = 'abcdabcd'
 
 const text = 'Matheus Nadai';
 const cipher = crypto.createCipher(alg, password)
+const crypted = cipher.update(text, 'utf8', 'hex')
+console.log(crypted);
